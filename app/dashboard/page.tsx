3016ae7590c7de19c3010bfe80f6437da1acbd72
Ax <13171754463@163.com>
2024-03-12 22:33:32 +0800

    删除错误代码重新部署

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,5 +2,7 @@ import { fetchRevenue } from '../lib/data';
 
 export default async function Page() {
   const result = await fetchRevenue();
+  console.log('dashboard');
+
   return <>dashboard</>;
 }
